47b7ea346346d76c3accffa79bff5dbbda5d4d40
ProDesignerNurul <maxnewsbd@gmail.com>
2022-10-29 14:02:25 +0600

    added a post on backend

--- src/App.js
+++ src/App.js
@@ -19,6 +19,15 @@ function App() {
     const email = event.target.email.value;
     const user = {name, email};
     console.log(user)
+    fetch('http://localhost:5000/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type' : 'application/json',
+      },
+      body: JSON.stringify(user)
+    })
+    .then( res => res.json())
+    .then( data => console.log(data))
     event.target.reset();
   }
 
